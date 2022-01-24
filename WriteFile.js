95959cf9ae46ee339d50ba124a23dc28d2c73f31
Ya-Men <96357593+Ya-Men@users.noreply.github.com>
2022-01-25 00:13:59 +0200

    Comments added and cleaning code

--- WriteFile.js
+++ WriteFile.js
@@ -16,32 +16,33 @@ Interceptor.attach(writeFile, {
     );
     */
     
-    /* 
-     Not that useful as JS vars does not acts as a pointer
-     So any assignment here is static only and will not affect the code flow
-    */
+
     var hFile = args[0],
     lpBuffer = args[1],
     nNumberOfBytesToWrite = args[2],
     lpNumberOfBytesWritten = args[3],
     lpOverlapped = args[4];
     
-    console.log("\nargs[0] -> HANDLE: " + hFile);
-    console.log("args[1] -> lpBuffer Content: " + Memory.readCString(lpBuffer));
-    console.log("args[2] -> nNumberOfBytesToWrite: " + nNumberOfBytesToWrite);
-    console.log("args[3] -> lpNumberOfBytesWritten: " + lpNumberOfBytesWritten);
-    console.log("args[4] -> lpOverlapped: " + lpOverlapped);
+    // console.log("\nargs[0] -> HANDLE: " + hFile);
+    // console.log("args[1] -> lpBuffer Content: " + Memory.readCString(lpBuffer));
+    // console.log("args[2] -> nNumberOfBytesToWrite: " + nNumberOfBytesToWrite);
+    // console.log("args[3] -> lpNumberOfBytesWritten: " + lpNumberOfBytesWritten);
+    // console.log("args[4] -> lpOverlapped: " + lpOverlapped);
     
     
-    // Allocte memory and change the buffer content
+    /* 
+      Allocte memory in the heap and change the buffer content (Not so stable)
+    */
     var newTextArr = [0x41, 0x42, 0x43, 0x44, 0x45]; // byteArray text
-    var textArrSize = Memory.alloc(newTextArr.length); // Dynamiclly allocate memory
+    var textArrSize = Memory.alloc(newTextArr.length); // Dynamiclly allocate memory at the HEAP
     textArrSize.writeByteArray(newTextArr); // Write byteArray to teh allocated memory
     args[1] = textArrSize; // Point lpBuffer to our newly created "string" in memory
- 
-    console.log("lpBuffer modified to: " + Memory.readCString( args[1])); 
-    // console.log("newText is:\n" + hexdump(lpBuffer) + "\n");
     
+    /* Or just use the line below */
+    //args[1].writeByteArray("newTextArr");
+
+ 
+    // console.log("lpBuffer modified to: " + Memory.readCString( args[1])); 
     console.log('Exit onEnter()\n');
 
   },
